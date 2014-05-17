d100b6fb4bccfa8b7145a4a6ca5fe67cfd06e821
Leigh Zhu <i@zhuli.me>
2014-05-17 19:44:30 +0800

    update dev basic

--- src/angular-swfupload.js
+++ src/angular-swfupload.js
@@ -1,9 +1,19 @@
 angular.module('angular.swfupload', []).
 provider('uiSwfupload', function() {
     
+    this.$get = function() {
+
+    };
 }).
 directive('uiSwfupload', ['$document', '$window', 'uiSwfupload',
     function($document, $window, uiSwfupload) {
         
+
+        return {
+            priority: 10,
+            link: function(scope, elm, attrs) {
+                
+            }
+        }
     }
 ]);
