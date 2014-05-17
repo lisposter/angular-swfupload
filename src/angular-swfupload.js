e08c341e8654aa1907e20618476b25a58c345018
Leigh Zhu <i@zhuli.me>
2014-05-17 19:14:52 +0800

    add files

---
+++ src/angular-swfupload.js
@@ -0,0 +1,9 @@
+angular.module('angular.swfupload', []).
+provider('uiSwfupload', function() {
+    
+}).
+directive('uiSwfupload', ['$document', '$window', 'uiSwfupload',
+    function($document, $window, uiSwfupload) {
+        
+    }
+]);
