dd97d4a2e406fe823dd74a59de07c027758f2569
Leigh Zhu <i@zhuli.me>
2014-05-20 09:16:40 +0800

    add config

--- src/angular-swfupload.js
+++ src/angular-swfupload.js
@@ -1,20 +1,32 @@
 angular.module('angular.swfupload', []).
-provider('uiSwfupload', function() {
-    
-    this.$get = function() {
+provider('uiSwfuploadOptions', function() {
+
+    var defaultOptions = {
 
     };
+
+    this.setOption = function(options) {
+        angular.extend(defaultOptions, options);
+    }
+    
+    this.$get = function() {
+        return defaultOptions;
+    };
 }).
-directive('uiSwfupload', ['$document', '$window', 'uiSwfupload',
-    function($document, $window, uiSwfupload) {
+directive('uiSwfupload', ['$document', '$window', 'uiSwfuploadOptions',
+    function($document, $window, uiSwfuploadOptions) {
         
+        var defaultOptions = uiSwfuploadOptions || {};
 
         return {
             priority: 10,
             link: function(scope, elm, attrs) {
                 if ( attrs.swfOption ) {
-			        var swfOption = scope.$parent.$eval(attrs.swfOption);
+			        var swfOption = scope[attrs.swfOption]
+                    var options = angualr.extend(defaultOptions, swfOption);
+                    scope[attrs.swfuploadId] = new SWFUpload(options);
 			    }
+
             }
         }
     }
