a8f568520e6628b0598f523682db2f696310aa11
Leigh Zhu <i@zhuli.me>
2014-05-20 09:23:20 +0800

    update: add default config && use id attr to bind swfupload

--- src/angular-swfupload.js
+++ src/angular-swfupload.js
@@ -2,7 +2,41 @@ angular.module('angular.swfupload', []).
 provider('uiSwfuploadOptions', function() {
 
     var defaultOptions = {
+        upload_url: '',
+        file_post_name: 'file',
+        post_params: { },
 
+        flash_color: 'transparent',
+        file_size_limit : "30KB", // 100MB
+        file_types : "*.jpg;*.gif;*.png;*.jpeg",
+        //file_types_description : "Images",
+        file_upload_limit : "0",
+        file_queue_limit : "0",
+
+        // Event Handler Settings (all my handlers are in the Handler.js file)
+        // file_dialog_start_handler : uploadHandle.fileDialogStart,
+        // // file_queued_handler : fileQueued,
+        // file_queue_error_handler : uploadHandle.fileQueueError,
+        // file_dialog_complete_handler : uploadHandle.file404DialogComplete,
+        // upload_start_handler : uploadHandle.uploadStart,
+        // upload_progress_handler : uploadHandle.uploadProgress,
+        // upload_error_handler : uploadHandle.uploadError,
+        // upload_success_handler : uploadHandle.uploadSuccess,
+        // upload_complete_handler : uploadHandle.uploadComplete,
+
+        // Button Settings
+
+        button_image_url: "",
+        button_width: "180",
+        button_height: "180",
+
+        
+        // Flash Settings
+        flash_url : "../lib/swfupload.swf",
+        
+        
+        // Debug Settings
+        debug: false
     };
 
     this.setOption = function(options) {
@@ -23,8 +57,9 @@ directive('uiSwfupload', ['$document', '$window', 'uiSwfuploadOptions',
             link: function(scope, elm, attrs) {
                 if ( attrs.swfOption ) {
 			        var swfOption = scope[attrs.swfOption]
-                    var options = angualr.extend(defaultOptions, swfOption);
-                    scope[attrs.swfuploadId] = new SWFUpload(options);
+                    var options = angular.extend(defaultOptions, swfOption);
+                    options.button_placeholder_id = attrs.id;
+                    scope[attrs.id] = new SWFUpload(options);
 			    }
 
             }
